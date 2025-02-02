e087e2f288dc7ef98e3ad7abc2254685072330ce
Nouzen-s <ryuu.seryu@gmail.com>
2025-02-02 20:24:37 +0800

    add models

---
+++ app/Models/Galery.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Galery extends Model
+{
+    protected $table = 'galeri_sections';
+    protected $fillable = ['image'];
+}
