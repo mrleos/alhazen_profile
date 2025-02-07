7bccc36ea0c54a314dbe037137dfbd04231061ca
Nouzen-s <ryuu.seryu@gmail.com>
2025-02-07 22:44:20 +0800

    add detail page in db courses

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $table = 'courses';
-    protected $fillable = ['title', 'description', 'image'];
+    protected $fillable = ['title', 'description', 'image','detail_page'];
 }
